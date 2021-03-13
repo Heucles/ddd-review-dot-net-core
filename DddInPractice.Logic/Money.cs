ce0a83b8c4d25d73c49fe653c215041b5a567841
Heucles Del Bianco Pelegia Jr <heucles.bianco@xpi.com.br>
2021-03-13 18:12:42 -0300

    Re factored the code and created the Money class to make the code simpler

--- DddInPractice.Logic/Money.cs
+++ DddInPractice.Logic/Money.cs
@@ -2,5 +2,32 @@ namespace DddInPractice.Logic
 {
     public class Money
     {
+        public int OneCentCount { get; private set; }
+        public int TenCentCount { get; private set; }
+        public int QuarterCount { get; private set; }
+        public int FiveDollarCount { get; private set; }
+        public int TwentyDollarCount { get; private set; }
+
+        public Money(int oneCentCount, int tenCentCount, int quarterCount, int fiveDollarCount, int twentyDollarCount)
+        {
+            OneCentCount = oneCentCount;
+            TenCentCount = tenCentCount;
+            QuarterCount = quarterCount;
+            FiveDollarCount = fiveDollarCount;
+            TwentyDollarCount = twentyDollarCount;
+        }
+
+        public static Money operator +(Money money1, Money money2)
+        {
+            Money sum = new Money(
+             money1.OneCentCount + money2.OneCentCount,
+             money1.TenCentCount + money2.TenCentCount,
+             money1.QuarterCount + money2.QuarterCount,
+             money1.FiveDollarCount + money2.FiveDollarCount,
+             money1.TwentyDollarCount + money2.TwentyDollarCount
+            );
+
+            return sum;
+        }
     }
 }
